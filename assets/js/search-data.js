70d8ad86929c5ac774af6e23e35e470e27005803
jameswathomson <52462116+jameswathomson@users.noreply.github.com>
2025-04-30 01:42:45 +0000

    Deploying to gh-pages from @ jameswathomson/jameswathomson.github.io@25914cad6504c3a6b8606ced4601e52ec3733069 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -143,11 +143,11 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-email',
-        title: 'email',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("https://github.com/jameswathomson", "_blank");
         },
       },{
         id: 'social-linkedin',
@@ -164,18 +164,18 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=", "_blank");
-        },
-      },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
         handler: () => {
           window.open("https://twitter.com/JamesWAThomson", "_blank");
+        },
+      },{
+        id: 'social-youtube',
+        title: 'YouTube',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://youtube.com/@youtube.com/@EchoesofPerception", "_blank");
         },
       },{
       id: 'light-theme',
